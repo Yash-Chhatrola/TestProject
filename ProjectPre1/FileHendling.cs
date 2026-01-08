27214cbc7b0fd17d950d971cb0aa891ec3237750
Yash Chhatrola <chhatrolayash@gmail.com>
2026-01-08 12:18:24 +0530

    newprojectadd

---
+++ ProjectPre1/FileHendling.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ProjectPre1
+{
+    internal class FileHendling
+    {
+        internal String path = "d://patel.txt";
+        internal void createFile()
+        {
+            try
+            {
+                if (File.Exists(path))
+                {
+                    Console.WriteLine("File Already Exist");
+                }
+                else
+                {
+                    FileStream fs = File.Create(path);
+                    fs.Close();
+                    Console.WriteLine("File Created Successfuly");
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+        }
+        internal void addContentOnFile()
+        {
+            try
+            {
+                StreamWriter fw = new StreamWriter(path);
+                fw.WriteLine("my Name is Chhatrola Yash Kishorbhai");
+                fw.Close();
+                Console.WriteLine("Data Save Successfuly");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+        }
+        internal void appendContentOnFile()
+        {
+            try
+            {
+                StreamWriter fw = new StreamWriter(path, true);
+                fw.WriteLine("I am working on C# in .net");
+                fw.Close();
+                Console.WriteLine("Data Save Successfuly");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+        }
+        internal void readDataOnFile()
+        {
+            try
+            {
+                StreamReader sr = new StreamReader(path);
+                String data = sr.ReadToEnd();
+                Console.WriteLine(data);
+                sr.Close();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+        }
+
+        internal void deleteFileOperation()
+        {
+            try
+            {
+                File.Delete(path);
+                Console.WriteLine("detlete file successfuly");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+        }
+    }
+    //internal class Program
+    //{
+    //    public static void Main(string[] args)
+    //    {
+    //        FileHendling obj = new FileHendling();
+    //        obj.createFile();
+    //        //obj.addContentOnFile();
+    //        obj.appendContentOnFile();
+    //        obj.readDataOnFile();
+    //        obj.deleteFileOperation();
+    //    }
+    //}
+}
---
+++ ProjectPre1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectPre1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //FileHendling obj = new FileHendling();
+            //obj.createFile();
+            //obj.addContentOnFile();
+            //////obj.appendContentOnFile();
+            //obj.readDataOnFile();
+            ////obj.deleteFileOperation();
+
+            ShortFileHendling sf = new ShortFileHendling();
+            sf.createFile();
+            sf.addContentOnFile();
+            ////obj.appendContentOnFile();
+            sf.readDataOnFile();
+            //obj.deleteFileOperation();
+        }
+    }
+}
